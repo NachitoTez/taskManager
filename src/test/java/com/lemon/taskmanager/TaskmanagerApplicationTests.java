c315c4183a458dcb44027f98b20f942225c02668
NachitoTez <ignacio.ramirezguembe@gmail.com>
2025-07-19 16:14:21 -0300

    Configuracion inicial
    Elimino archivos autogenerados
    
    Remove .DS_Store and ignore it

---
+++ src/test/java/com/lemon/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,9 @@
+package com.lemon.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+}
